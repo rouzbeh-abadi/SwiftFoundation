3788d2436e26f2b1bf35136ae7f77d3e238cbda7
rouzbeh abadi <rouzbeh.abadi@piavita.com>
2022-02-26 00:51:56 +0100

    Add a snapshot strategy

---
+++ Sources/Testability/SnapshotTesting/Strategy/windowedImage.swift
@@ -0,0 +1,21 @@
+
+import Foundation
+
+extension Snapshotting where Value: UIViewController, Format == UIImage {
+    public static var windowedImage: Snapshotting {
+        return Snapshotting<UIImage, UIImage>.image.asyncPullback { vc in
+            Async<UIImage> { callback in
+                UIView.setAnimationsEnabled(false)
+                let window = UIApplication.shared.windows[0]
+                window.rootViewController = vc
+                DispatchQueue.main.async {
+                    let image = UIGraphicsImageRenderer(bounds: window.bounds).image { ctx in
+                        window.drawHierarchy(in: window.bounds, afterScreenUpdates: true)
+                    }
+                    callback(image)
+                    UIView.setAnimationsEnabled(true)
+                }
+            }
+        }
+    }
+}
