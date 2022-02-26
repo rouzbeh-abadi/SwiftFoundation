da0e0622243c14ab4facc09d6285a2799ff54760
rouzbeh abadi <rouzbeh.abadi@piavita.com>
2022-02-26 01:54:00 +0100

    Add Device height for UITests

---
+++ Sources/SwiftFoundation/Source/DeviceType/DeviceHeight.swift
@@ -0,0 +1,18 @@
+#if !os(macOS)
+import UIKit
+
+public struct DeviceHeight {
+    
+    static let iPhoneSE1: CGFloat = 568
+    static let iPhone8: CGFloat = 667
+    static let iPhone13: CGFloat = 884
+    static let iPhone13ProMax: CGFloat = 926
+
+    
+    static let iPadMini: CGFloat = 744
+    static let iPad9_7: CGFloat = 768
+    static let iPad11: CGFloat = 834
+    static let iPad12_9: CGFloat = 1024
+    
+}
+#endif
